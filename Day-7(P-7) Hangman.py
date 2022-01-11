8b1015a79aaae6850d86451444f0ecc6a57ca6ec
Alistair Saldanha <shaanalistair47@gmail.com>
2022-01-11 22:38:12 +0530

    Hangman Commit

--- Day-7(P-7) Hangman.py
+++ Day-7(P-7) Hangman.py
@@ -14,17 +14,82 @@ word_len = len(chosen_word)
 display = []
 for i in range(word_len):
     display += "_"
-print(display)
+print(f"{' '.join(display)}")
 # condition to terminate while loop
 end_of_game = False
-
+stages = [
+'''
+    +---+
+    |   |
+    O   |
+   /|\  |
+   / \  |
+ ==========
+''',
+'''              
+    +---+
+    |   |
+    O   |
+   /|\  |
+   /    |
+ ==========
+ ''',
+ '''
+    +---+
+    |   |
+    O   |
+   /|\  |
+        |
+ ==========
+ ''',
+ '''
+    +---+
+    |   |
+    O   |
+   /|   |
+        |
+ ==========
+ ''',
+ '''
+    +---+
+    |   |
+    O   |
+    |   |
+        |
+ ==========
+ ''',
+ '''
+    +---+
+    |   |
+    O   |
+        |
+        |
+ ==========
+ ''',
+ '''
+    +---+
+    |   |
+        |
+        |
+        |
+ ==========
+'''
+]
+lives = 6
 while not end_of_game:
     guess_letter = input("Guess a letter: ").lower()
     for index in range(word_len):
         letter = chosen_word[index]
         if(letter == guess_letter.lower()):
             display[index] = letter
-    print(display)
+    print(f"{' '.join(display)}")
+    if guess_letter not in chosen_word:
+        lives -= 1
+        print(stages[lives])
+        if lives == 0:
+            end_of_game = True
+            print("Sorry! You Lose the game.")
     if "_" not in display:
         end_of_game = True
-        print(f"Congratulations! Solution is {chosen_word}")
+        print(f"Congratulations! The word is {chosen_word}")
+
